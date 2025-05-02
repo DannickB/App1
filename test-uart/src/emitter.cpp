ab580c2eccca529fc5c12c5fd171826a4b3ce524
DannickB <bild2707@usherbrooke.ca>
2025-05-02 13:33:38 -0400

    platform.ini a pas push

--- test-uart/src/emitter.cpp
+++ test-uart/src/emitter.cpp
@@ -1,16 +1,16 @@
-#include <Arduino.h>
-#include <HardwareSerial.h>
-HardwareSerial ESP32Serial1(1);
-int i = 0;
-
-void setup() {
+// #include <Arduino.h>
+// #include <HardwareSerial.h>
+// HardwareSerial ESP32Serial1(1);
+// int i = 0;
+// 
+// void setup() {
   // put your setup code here, to run once:
-  ESP32Serial1.begin(9600, SERIAL_8N1, 19, 18);
-}
-
-void loop() {
+  // ESP32Serial1.begin(9600, SERIAL_8N1, 36, 37);
+// }
+// 
+// void loop() {
   // put your main code here, to run repeatedly:
-  i++;
-  std::string msg = std::to_string(i); 
-  ESP32Serial1.write(msg.c_str());
-}
+  // i++;
+  // std::string msg = std::to_string(i); 
+  // ESP32Serial1.write(msg.c_str());
+// }

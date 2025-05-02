151a4c855996eb5f031664127c4e553d519bd8d7
DannickB <bild2707@usherbrooke.ca>
2025-05-02 13:22:37 -0400

    uart test s3

--- test-uart/src/receiver.cpp
+++ test-uart/src/receiver.cpp
@@ -7,15 +7,17 @@ int msg = 0;
 
 void setup() {
   // put your setup code here, to run once:
-  SerialPort.begin(9600, SERIAL_8N1, 18, 19);
+  SerialPort.begin(9600, SERIAL_8N1, 37, 36);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  bitOfMessage = char(SerialPort.read());
-  if (bitOfMessage != '\n'){
-    message += bitOfMessage;
+  if(SerialPort.available()){
+    bitOfMessage = char(SerialPort.read());
+    if (bitOfMessage != '\n'){
+      message += bitOfMessage;
+    }
+    msg = std::stoi(message.substr(2,1));
+    Serial.println(msg);
   }
-  msg = std::stoi(message.substr(2,1));
-  Serial.println(msg);
 }
